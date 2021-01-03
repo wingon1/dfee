cec5cc79c259d998a5ab27b6c4402191a4c383d2
Hansol Ji <hansolji@Hansolui-MacBookPro.local>
2021-01-03 21:26:07 +0900

    -

--- src/main/java/com/example/jidudu/controller/MainController.java
+++ src/main/java/com/example/jidudu/controller/MainController.java
@@ -1,15 +1,30 @@
 package com.example.jidudu.controller;
 
+import com.example.jidudu.dbms.User;
+import com.example.jidudu.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
 public class MainController {
-        @GetMapping
-        public String hello()
-        {
-            return "hello";
-        }
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public ModelAndView hello()
+    {
+        Iterable<User> user = userService.findAll();
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("hello");
+		mv.addObject("user", user);
+		
+		return mv;
+    }
 }
--- src/main/java/com/example/jidudu/dbms/User.java
+++ src/main/java/com/example/jidudu/dbms/User.java
@@ -2,12 +2,7 @@ package com.example.jidudu.dbms;
 
 import javax.persistence.*;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Entity
-@Getter
-@Setter
 public class User {
     
     @Id
@@ -17,9 +12,85 @@ public class User {
     @Column(name="user_id")
     private String userId;
 
-    @Column
+    @Column(name="password")
     private String password;
 
-    @Column
+    @Column(name="name")
     private String name;
+
+
+    public User() {
+    }
+
+    public User(Long idx, String userId, String password, String name) {
+        this.idx = idx;
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+    }
+
+    public Long getIdx() {
+        return this.idx;
+    }
+
+    public void setIdx(Long idx) {
+        this.idx = idx;
+    }
+
+    public String getUserId() {
+        return this.userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User idx(Long idx) {
+        this.idx = idx;
+        return this;
+    }
+
+    public User userId(String userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public User password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public User name(String name) {
+        this.name = name;
+        return this;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+            " idx='" + getIdx() + "'" +
+            ", userId='" + getUserId() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", name='" + getName() + "'" +
+            "}";
+    }
+
+    
 }
